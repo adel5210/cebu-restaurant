1a9a19f3929451778462439c5e7ba3dc9dff4583
adel <adel.albediwy@gmail.com>
2023-02-23 07:02:09 +0300

    Initial compiled subtree commit

---
+++ js/about.5b0ca5b3.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcebu_restaurant"]=self["webpackChunkcebu_restaurant"]||[]).push([[443],{7750:function(t,u,n){n.r(u),n.d(u,{default:function(){return l}});var e=function(){var t=this;t._self._c;return t._m(0)},s=[function(){var t=this,u=t._self._c;return u("div",{staticClass:"about"},[u("h1",[t._v("This is an about page")])])}],a=n(1001),r={},c=(0,a.Z)(r,e,s,!1,null,null,null),l=c.exports}}]);
+//# sourceMappingURL=about.5b0ca5b3.js.map
