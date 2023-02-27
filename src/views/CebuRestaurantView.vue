17ff8345e3587870e9b5e29b4ab6399dd069cba8
adel5210 <adel.albediwy@gmail.com>
2023-02-27 16:50:04 +0300

    Configure Rating

--- src/views/CebuRestaurantView.vue
+++ src/views/CebuRestaurantView.vue
@@ -1,48 +1,53 @@
 <template>
   <v-content style="width: 100%; height: 100%;">
     <GmapMap
+        v-if="centerMapPosition"
         id="map"
         ref="mapRef"
-        v-if="cebuPosition"
-        :center="cebuPosition"
+        :center="centerMapPosition"
+        :style="googleMapSIze"
         :zoom="15"
         map-type-id="terrain"
-        :style="googleMapSIze"
     >
       <GmapMarker v-for="(plot, index) in filteredRestaurantPlots"
                   :key="index"
-                  @click="openInfoWindowTemplate(plot)"
+                  ref="mapMarkers"
                   :icon="pinSymbol(plot.colorMarker)"
                   :position="google && new google.maps.LatLng(plot.position[0], plot.position[1])"
+                  @click="openInfoWindowTemplate(plot)"
       />
       <gmap-info-window
+          :opened="infoWindow.open"
           :options="{maxWidth: 300, pixelOffset: { width: 0, height: -35 }}"
           :position="infoWindow.position"
-          :opened="infoWindow.open"
           @closeclick="infoWindow.open=false">
         <div v-html="infoWindow.template"></div>
       </gmap-info-window>
-
-      <!--      <GmapMarker v-if="hasEnableUserMarker"-->
-      <!--                  :icon="mapMarkerIcon1"-->
-      <!--                  :position="google && new google.maps.LatLng(userPlots.lat, userPlots.lng)"-->
-      <!--      />-->
-
+      <GmapPolygon
+          v-if="showPolygon"
+          ref="mapPolygon"
+          :draggable="true"
+          :editable="true"
+          :paths="userPolygonPath"
+          @paths_changed="updatePolygon($event)"
+      >
+      </GmapPolygon>
       <DirectionsRenderer
           v-if="end"
-          travelMode="DRIVING"
+          :destination="destionation"
           :origin="origin"
-          :destination="destionation"/>
+          travelMode="DRIVING"/>
     </GmapMap>
-    <v-bottom-navigation v-model="selectedBottomNavbar" ref="bttmNav" height="50" light grow>
-      <v-menu top
+
+    <v-bottom-navigation ref="bttmNav" v-model="selectedBottomNavbar" background-color="#F5F5F5" grow height="50" light>
+      <v-menu :close-on-click="showRestaurantTypes"
               :offset-y=true
-              :close-on-click="showRestaurantTypes">
+              top>
         <template v-slot:activator="{ on, attrs }">
-          <v-btn value="restaurant_types" x-large
-                 @click="showRestaurantTypes !== showRestaurantDetail"
-                 v-bind="attrs"
-                 v-on="on">
+          <v-btn v-bind="attrs" v-on="on"
+                 value="restaurant_types"
+                 x-large
+                 @click="showRestaurantTypes !== showRestaurantDetail">
             <span>Restaurant types</span>
             <v-icon>mdi-list-box-outline</v-icon>
           </v-btn>
@@ -50,37 +55,46 @@
 
         <v-list>
           <v-list-item v-for="(item, index) in restaurantTypes"
+                       :key="index"
                        link
-                       @click="selectedRestaurantType = item"
-                       :key="index">
+                       @click="selectedRestaurantType = item">
             <v-list-item-title v-text="item.type"/>
           </v-list-item>
         </v-list>
       </v-menu>
 
-      <v-btn value="goto" x-large v-if="showGoNow " @click="rerouteToRestaurant">
+      <v-btn value="show_polygon" x-large
+             @click="spawnPolygon()">
+        <span>Draw</span>
+        <v-icon>mdi-vector-polygon-variant</v-icon>
+      </v-btn>
+
+      <v-btn v-if="showGoNow " value="goto" x-large @click="rerouteToRestaurant">
         <span>Go now</span>
         <v-icon>mdi-map-marker-radius-outline</v-icon>
       </v-btn>
-      <v-btn value="reset" x-large v-if="!showGoNow && null!=selectedRp.name" @click="resetRoute">
+      <v-btn v-if="!showGoNow && null!=selectedRp.name" value="reset" x-large @click="resetRoute(true)">
         <span>Reset</span>
         <v-icon>mdi-map-marker-radius-outline</v-icon>
       </v-btn>
 
-      <v-btn value="show_details" x-large v-if="showGoNow" @click="showRestaurantDetail=!showRestaurantDetail">
+      <v-btn v-if="showGoNow" value="show_details" x-large @click="showRestaurantDetail=!showRestaurantDetail">
         <span>Show details</span>
         <v-icon>mdi-silverware</v-icon>
       </v-btn>
 
+      <v-btn v-if="showPolygon" value="count_polygon"
+             x-large>
+        <strong>Count: {{ countRestaurants }}</strong>
+      </v-btn>
 
     </v-bottom-navigation>
-
     <v-bottom-sheet
         v-model="showRestaurantDetail"
-        inset
         hide-overlay
+        inset
     >
-      <v-card style="margin-bottom: 50px" v-if="showRestaurantDetail">
+      <v-card v-if="showRestaurantDetail" style="margin-bottom: 70px">
         <v-card-title>{{ selectedRp.name }}</v-card-title>
         <v-card-text>
           Type: {{ restaurantTypes.find(f => f.id === selectedRp.restaurant_type).type }}
@@ -89,23 +103,26 @@
           <br>
           Special Meals: {{ selectedRp.special_meals }}
           <br>
+          Visited count: {{ selectedRp.visited_count }}
+          <br>
           Ratings: {{ selectedRp.rating }}
         </v-card-text>
         <v-card-actions>
-          <section style="display: inline-flex; align-items: center" v-if="!selectedRp.setVisited">
+          <section v-if="!selectedRp.setVisited" style="display: inline-flex; align-items: center">
             Have you visited this place?
             <v-spacer></v-spacer>
             <section v-if="null==selectedRp.setVisited">
-              <v-btn outlined
+              <v-btn :loading="showVisitLoading"
                      class="mr-2 ml-2"
-                     @click="onVisited(true)"
+                     outlined
                      rounded
+                     @click="onVisited(true)"
               >Yes
               </v-btn>
               <v-btn
                   outlined
                   rounded
-                  @click="onVisited(false)"
+                  @click="onVisited( false)"
               >No
               </v-btn>
             </section>
@@ -117,8 +134,9 @@
             Give it a rate
             <v-spacer></v-spacer>
             <v-rating
-                class="ml-2"
                 v-model="selectedRp.setRating"
+                @input="onRating"
+                class="ml-2"
                 color="yellow accent-4"
                 dense
                 hover
@@ -128,11 +146,11 @@
 
         <v-card-actions>
           <v-btn
-              raised
               block
-              large
               depressed
+              large
               outlined
+              raised
               @click="resetDetails"
           >CLOSE
           </v-btn>
@@ -140,6 +158,12 @@
       </v-card>
 
     </v-bottom-sheet>
+    <v-footer
+        :padless="true"
+        height="20"
+        style="justify-content: center"
+    >Created by: Adel Mahmoud Sadek - <strong> Cebu Restaurants </strong> - 2023
+    </v-footer>
   </v-content>
 </template>
 
@@ -158,11 +182,10 @@ export default {
     return {
       hasEnableUserMarker: false,
 
-      cebuPosition: {lat: 1, lng: 1},
+      centerMapPosition: {lat: 1, lng: 1},
       restaurantPlots: [],
       restaurantTypes: [],
       userPlots: {lat: 1, lng: 1},
-      nextPageTokenApi: null,
       start: "",
       end: "",
 
@@ -180,19 +203,26 @@ export default {
 
       selectedRestaurant: {},
       selectedRp: {
+        id: null,
         name: null,
         address: null,
         rating: null,
         special_meals: null,
         restaurant_type: null,
         setVisited: null,
-        setRating: null
+        setRating: null,
+        visited_count: null
       },
       selectedRestaurantType: null,
-      
+
       showGoNow: false,
       showRestaurantDetail: false,
-      showRestaurantTypes: false
+      showRestaurantTypes: false,
+      showVisitLoading: false,
+
+      showPolygon: false,
+      userPolygonPath: [],
+      countRestaurants: 0
 
     }
   },
@@ -205,7 +235,7 @@ export default {
       let request = {
         type: ['restaurant'],
         radius: 5500,
-        location: this.cebuPosition
+        location: this.centerMapPosition
       };
 
       if (null != temp_next_page_token) {
@@ -215,17 +245,8 @@ export default {
       console.log(request);
 
       let service = new this.google.maps.places.PlacesService(map);
-      service.nearbySearch(request, (results, status, next_page_token) => {
+      service.nearbySearch(request, (results, status) => {
         if (status === 'OK') {
-          console.log(results);
-          console.log(next_page_token);
-          // setTimeout(() => {
-          //   if (next_page_token.hasNextPage) {
-          //     temp_next_page_token = next_page_token;
-          //     console.log(next_page_token);
-          //     this.retrieveNearbyRestaurantsGoogleApi(map, temp_next_page_token);
-          //   }
-          // }, 3000);
           for (let i = 0; i < results.length; i++) {
             this.restaurantPlots.push({
               position: [results[i].geometry.location.lat(), results[i].geometry.location.lng()]
@@ -237,7 +258,7 @@ export default {
     async retrieveRestaurantTypes() {
       const {data} = await supabase.from('restaurant_type')
           .select('id,type')
-      console.log(data);
+      // console.log(data);
       this.restaurantTypes = data;
     },
     async retrieveSpecialMeals(id) {
@@ -245,42 +266,61 @@ export default {
           .select('name')
           .eq('restaurant', id);
     },
+    async retrieveVisitedRestaurant(id) {
+      return supabase
+          .from('visited_restaurant')
+          .select('id')
+          .eq('restaurant', id);
+    },
+    async retrieveRatingRestaurant(id) {
+      return supabase
+          .from('rating_restaurant')
+          .select('id,rating')
+          .eq('restaurant', id);
+    },
     async retrieveRestaurants() {
       const {data} = await supabase.from('restaurant')
           .select()
-      console.log(data)
       for (let i = 0; i < data.length; i++) {
         this.restaurantPlots.push({
           id: data[i].id,
           position: [Number(data[i].lat), Number(data[i].lng)],
           name: data[i].name,
           address: data[i].address,
-          rating: data[i].rating,
+          rating: null,
           colorMarker: 'red',
           restaurant_type: data[i].restaurant_type,
           setVisited: null,
-          setRating: null
+          setRating: null,
+          visited_count: null
         });
       }
-      console.log(this.restaurantPlots);
 
     },
     async retrieveDefaultConfig() {
       const {data} = await supabase.from('default_config')
           .select()
           .eq('id', 1)
-      console.log(data);
-      this.cebuPosition = {
+      // console.log(data);
+      this.centerMapPosition = {
         lat: data[0].lat,
         lng: data[0].lng
       }
 
     },
     async openInfoWindowTemplate(selectedMarker) {
-      console.log(this.restaurantTypes)
       let rp = selectedMarker;
       let rp_sm = await this.retrieveSpecialMeals(rp.id);
       selectedMarker.special_meals = rp_sm.data.map(m => m.name).join(", ")
+      let visited_count = await this.retrieveVisitedRestaurant(rp.id);
+      selectedMarker.visited_count = visited_count.data.length;
+      let ratings_restaurant = await this.retrieveRatingRestaurant(rp.id);
+      selectedMarker.rating = 0;
+      for (let i = 0; i < ratings_restaurant.data.length; i++) {
+        selectedMarker.rating = ratings_restaurant.data[i].rating / (i + 1);
+      }
+      console.log(selectedMarker.rating)
+
       for (let i = 0; i < this.restaurantPlots.length; i++) {
         this.restaurantPlots[i].colorMarker = 'red'
       }
@@ -291,13 +331,15 @@ export default {
                                   ${rp.address}`
       this.infoWindow.open = true
       this.showGoNow = true;
+
+
       this.selectedRp = rp;
     },
     screenResize() {
       this.windowSize = {x: window.innerWidth, y: window.innerHeight}
     },
     readjustSize() {
-      this.googleMapSIze = 'width: 100%; height:' + (this.windowSize.y - 50) + 'px';
+      this.googleMapSIze = 'width: 100%; height:' + (this.windowSize.y - (50 + 20)) + 'px';
     },
     currentLocation() {
       navigator.geolocation.getCurrentPosition(
@@ -307,7 +349,6 @@ export default {
               lng: position.coords.longitude
             }
             this.hasEnableUserMarker = true;
-            console.log(this.userPlots);
             this.start = this.google && new this.google.maps.LatLng(this.userPlots.lat, this.userPlots.lng);
           },
           error => {
@@ -321,18 +362,22 @@ export default {
       this.end = this.google && new this.google.maps.LatLng(rp.position[0], rp.position[1]);
       this.showGoNow = false;
     },
-    resetRoute() {
+    resetRoute(withSelectedRp) {
       this.start = null;
       this.end = null;
-      this.selectedRp = {
-        name: null,
-        address: null,
-        rating: null,
-        special_meals: null,
-        restaurant_type: null,
-        setRating: null,
-        setVisited: null
-      };
+      if (withSelectedRp) {
+        this.selectedRp = {
+          id: null,
+          name: null,
+          address: null,
+          rating: null,
+          special_meals: null,
+          restaurant_type: null,
+          setRating: null,
+          setVisited: null,
+          visited_count: null
+        };
+      }
       this.infoWindow = {
         position: {lat: 0, lng: 0},
         open: false,
@@ -353,13 +398,76 @@ export default {
         scale: 1
       };
     },
-    onVisited(hasVisit) {
+    async onVisited(hasVisit) {
       this.selectedRp.setVisited = hasVisit
+      if (hasVisit) {
+        this.showVisitLoading = true;
+        this.currentLocation();
+        const {data, error} = await supabase
+            .from('visited_restaurant')
+            .insert([
+              {
+                restaurant: this.selectedRp.id,
+                user_lat: String(this.userPlots.lat),
+                user_lng: String(this.userPlots.lng)
+              },
+            ])
+        this.selectedRp.visited_count = this.selectedRp.visited_count + 1;
+        if (data || error) {
+          if (error) {
+            console.error(error)
+          }
+        }
+      }
+      this.showVisitLoading = false;
+    },
+    async onRating() {
+      const {data, error} = await supabase
+          .from('rating_restaurant')
+          .insert([
+            {restaurant: this.selectedRp.id, rating: this.selectedRp.setRating},
+          ])
+      if (data || error) {
+        if (error) {
+          console.error(error)
+        }
+      }
     },
     resetDetails() {
       this.showRestaurantDetail = false;
       this.selectedRp.setRating = null;
       this.selectedRp.setVisited = null;
+    },
+    spawnPolygon() {
+      this.showPolygon = !this.showPolygon;
+      if (this.showPolygon) {
+        const centerMapPosition = {
+          lat: this.$refs.mapRef.$mapObject.center.lat(),
+          lng: this.$refs.mapRef.$mapObject.center.lng()
+        }
+        this.userPolygonPath = [
+          {lat: centerMapPosition.lat - 0.003, lng: centerMapPosition.lng - 0.003},
+          {lat: centerMapPosition.lat + 0.003, lng: centerMapPosition.lng - 0.003},
+          {lat: centerMapPosition.lat + 0.003, lng: centerMapPosition.lng + 0.003},
+          {lat: centerMapPosition.lat - 0.003, lng: centerMapPosition.lng + 0.003},
+        ]
+      }
+
+    },
+    updatePolygon() {
+
+      // console.log(mvcArray)
+      this.countRestaurants = 0;
+      // console.log(this.$refs.mapPolygon)
+      this.$refs.mapPolygon.$polygonPromise.then(polygon => {
+        for (let i = 0; i < this.$refs.mapMarkers.length; i++) {
+          this.$refs.mapMarkers[i].$markerPromise.then(mark => {
+            if (this.google.maps.geometry.poly.containsLocation(mark.getPosition(), polygon)) {
+              this.countRestaurants = this.countRestaurants + 1
+            }
+          })
+        }
+      })
     }
   },
   computed: {
@@ -372,18 +480,16 @@ export default {
       if (!this.end) return null;
       return this.end;
     },
-    filteredRestaurantPlots(){
-      return null!==this.selectedRestaurantType ?
+    filteredRestaurantPlots() {
+      this.resetRoute(false);
+      return null !== this.selectedRestaurantType ?
           this.restaurantPlots.filter(f => f.restaurant_type === this.selectedRestaurantType.id)
           : this.restaurantPlots;
-      
-    }
+
+    },
+
   },
-  watch: {
-    nextPageTokenApi(newT) {
-      console.log(newT)
-    }
-  },
+  watch: {},
   mounted() {
     this.screenResize();
     this.readjustSize();
